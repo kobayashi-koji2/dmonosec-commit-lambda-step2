b39f16e22ddb49948e26e7ebf5df35d095da3377
Munefumi YAMASHITA <mun-yamashita@design.secom.co.jp>
2024-02-05 10:27:33 +0900

    auth.pyのトークンからユーザー情報を取得する処理を修正

--- _local-dev-files/layer/common_functions_layer/python/auth.py
+++ _local-dev-files/layer/common_functions_layer/python/auth.py
@@ -4,6 +4,7 @@ import os
 import boto3
 from aws_lambda_powertools import Logger
 from jose import JWTError, jwt
+from boto3.dynamodb.conditions import Key
 
 import db
 import ssm
@@ -47,7 +48,7 @@ def _get_login_user(event, verify_password_exp=True):
         id_token = event["headers"]["Authorization"]
         claims = jwt.get_unverified_claims(id_token)
 
-        user_id = claims["custom:auth_id"]
+        auth_id = claims["custom:auth_id"]
 
         # 認証日時
         auth_time = claims["auth_time"]
@@ -62,8 +63,24 @@ def _get_login_user(event, verify_password_exp=True):
     except (JWTError, KeyError, ValueError, TypeError) as e:
         raise AuthError(401, "認証情報が不正です。") from e
 
+    account_table = dynamodb.Table(ssm.table_names["ACCOUNT_TABLE"])
+    account = db.get_account_info(auth_id, account_table)
+    if not account:
+        raise AuthError(401, "認証情報が不正です。")
+
     user_table = dynamodb.Table(ssm.table_names["USER_TABLE"])
-    login_user = db.get_user_info_by_user_id(user_id, user_table)
+    # TODO 1アカウント複数契約対応前の暫定処理
+    # TODO 本来はヘッダーから契約コードを取得してユーザー情報を取得する
+    res = user_table.query(
+        IndexName="account_id_index",
+        KeyConditionExpression=Key("account_id").eq(account["account_id"]),
+    ).get("Items", [])
+    user_list = [
+        item
+        for item in res
+        if item.get("user_data", {}).get("config", {}).get("del_datetime") is None
+    ]
+    login_user = user_list[0] if user_list else None
 
     if not login_user:
         raise AuthError(401, "認証情報が不正です。")
