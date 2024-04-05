56cd7ae515304654cc6e1902691e2ca34b25191a
鶴見 侑磨 <yuma-tsurumi@design.secom.co.jp>
2024-04-05 13:14:37 +0000

    Squashed commit of the following
    commit 4ec10bcb 
    Author: Yuma TSURUMI <yuma-tsurumi@design.secom.co.jp> 
    Date: Fri Apr 05 2024 22:13:56 GMT+0900 (日本標準時) 
    
        メール通知設定詳細修正

--- notification-detail/contents/validate.py
+++ notification-detail/contents/validate.py
@@ -8,15 +8,15 @@ logger = Logger()
 
 # パラメータチェック
 def validate(event, user_info, tables):
-    headers = event.get("headers", {})
     pathParam = event.get("pathParameters", {})
-    if not headers or not pathParam:
+    logger.debug(f"pathParam: {pathParam}")
+    if not pathParam:
         return {"message": "リクエストパラメータが不正です。"}
-    if "Authorization" not in headers or "device_id" not in pathParam:
+    if "device_id" not in pathParam:
         return {"message": "リクエストパラメータが不正です。"}
 
     device_id = event["pathParameters"]["device_id"]
-    logger.info(f"device_id: {device_id}")
+    logger.debug(f"device_id: {device_id}")
 
     # 1.3 ユーザー権限確認
     contract_info = db.get_contract_info(user_info["contract_id"], tables["contract_table"])
@@ -29,8 +29,8 @@ def validate(event, user_info, tables):
     # 2 デバイス操作権限チェック
     ##################
     device_info = db.get_device_info_other_than_unavailable(device_id, tables["device_table"])
-    logger.info(f"device_id: {device_id}")
-    logger.info(f"device_info: {device_info}")
+    logger.debug(f"device_id: {device_id}")
+    logger.debug(f"device_info: {device_info}")
     if not device_info:
         return {"message": "デバイス情報が存在しません。"}
 
@@ -46,8 +46,8 @@ def operation_auth_check(user_info, contract_info, device_id, tables):
     user_type, user_id = user_info["user_type"], user_info["user_id"]
     # 2.1 デバイスID一覧取得
     accunt_devices = contract_info["contract_data"]["device_list"]
-    logger.info(f"ユーザID:{user_id}")
-    logger.info(f"権限:{user_type}")
+    logger.debug(f"ユーザID:{user_id}")
+    logger.debug(f"権限:{user_type}")
     if device_id not in accunt_devices:
         return False
 
