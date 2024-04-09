136b0d97652f0fb55d45eb4ad0200a6cbd68e597
鶴見 侑磨 <yuma-tsurumi@design.secom.co.jp>
2024-04-09 11:46:12 +0000

    Squashed commit of the following
    commit 0ca0d076 
    Author: Yuma TSURUMI <yuma-tsurumi@design.secom.co.jp> 
    Date: Tue Apr 09 2024 19:44:33 GMT+0900 (日本標準時) 
    
        履歴データ保存Lambda修正

--- history-data-backup/contents/lambda_function.py
+++ history-data-backup/contents/lambda_function.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 import json
 import base64
 import logging
+import traceback
 from aws_lambda_powertools import Logger
  
 null = None
@@ -9,38 +10,42 @@ null = None
 logger = Logger()
 
 
-def lambda_handler(event, context):
-    output = []
-    for record in event['records']:
-        payload = base64.b64decode(record['data']).decode('utf8')
-        logger.debug(f"payload={payload}")
- 
-        ret_val = verify_if_expired(payload)
-        if ret_val == True:
-            output_record = {
-                'recordId': record['recordId'],
-                'result': 'Ok',
-                'data': record['data']
-            }
-            output.append(output_record)
- 
-        else:
-            output_record = {
-                'recordId': record['recordId'],
-                'result': 'Dropped',
-                'data': null
-            }
-            output.append(output_record)
- 
-    return {'records': output}
- 
- 
 def verify_if_expired(payload):
-    try:
-        parsed_json = json.loads(payload)
-        if str(parsed_json["eventName"]).upper() == "REMOVE":
-            if parsed_json.get("userIdentity").get("principalId") == "dynamodb.amazonaws.com":
-                return True
-    except Exception as e:
-        print(e,"error")
+    parsed_json = json.loads(payload)
+    if str(parsed_json["eventName"]).upper() == "REMOVE":
+        user_identity = parsed_json.get("userIdentity", {})
+        if user_identity and user_identity.get("principalId", {}) == "dynamodb.amazonaws.com":
+            return True
     return False
+
+
+def lambda_handler(event, context):
+    try:
+        output = []
+        for record in event['records']:
+            payload = base64.b64decode(record['data']).decode('utf8')
+            logger.debug(f"payload={payload}")
+
+            # TTL削除判定
+            ret_val = verify_if_expired(payload)
+            if ret_val == True:
+                output_record = {
+                    'recordId': record['recordId'],
+                    'result': 'Ok',
+                    'data': record['data']
+                }
+                output.append(output_record)
+    
+            else:
+                output_record = {
+                    'recordId': record['recordId'],
+                    'result': 'Dropped',
+                    'data': null
+                }
+                output.append(output_record)
+        return {'records': output}
+
+    except Exception as e:
+        logger.error(f"Exception: {e}")
+        logger.error(traceback.format_exc())
+ 
