233ff8a4c8ad8755ea0223c7ad20d4120a30963e
Munefumi YAMASHITA <mun-yamashita@design.secom.co.jp>
2024-04-17 17:18:53 +0900

    連動制御設定の不具合修正

--- automation-settings/contents/lambda_function.py
+++ automation-settings/contents/lambda_function.py
@@ -139,9 +139,11 @@ def lambda_handler(event, context, user_info, trigger_device_id, request_body):
             do_automation_item = {
                 "automation_id": item["automation_id"],
                 "automation_name": item["automation_name"],
-                "trigger_device_id": item["trigger_device_id"],
+                "control_device_id": item["control_device_id"],
                 "trigger_event_type": item["trigger_event_type"],
                 "trigger_terminal_no": item.get("trigger_terminal_no"),
+                "trigger_event_detail_state": item.get("trigger_event_detail_state"),
+                "trigger_event_detail_flag": item.get("trigger_event_detail_flag"),
                 "control_do_no": item["control_do_no"],
                 "control_di_state": item["control_di_state"],
             }
