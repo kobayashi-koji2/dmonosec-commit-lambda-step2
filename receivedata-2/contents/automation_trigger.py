c92eadabe1bb6909999eee2eff7f7998fb825642
Munefumi YAMASHITA <mun-yamashita@design.secom.co.jp>
2024-06-12 17:15:40 +0900

    接点入力状態のOn/Off反転

--- receivedata-2/contents/automation_trigger.py
+++ receivedata-2/contents/automation_trigger.py
@@ -5,15 +5,21 @@ logger = Logger()
 
 
 def diNameToEventDetail(terminal_state_name, device_info):
-    di_list = device_info.get("device_data", {}).get("config", {}).get("terminal_settings", {}).get("di_list", [])
+    di_list = (
+        device_info.get("device_data", {})
+        .get("config", {})
+        .get("terminal_settings", {})
+        .get("di_list", [])
+    )
     for di in di_list:
         if di.get("di_on_name") == terminal_state_name:
-            event_detail = 1
-            break
-        elif di.get("di_off_name") == terminal_state_name:
             event_detail = 0
             break
+        elif di.get("di_off_name") == terminal_state_name:
+            event_detail = 1
+            break
     return event_detail
+
 
 def automationTrigger(hist_list, device_info):
     logger.debug(f"automationTrigger開始")
@@ -21,14 +27,35 @@ def automationTrigger(hist_list, device_info):
     for hist in hist_list:
         # 接点入力変化
         if hist["hist_data"]["event_type"] == "di_change":
-            event_detail = diNameToEventDetail(hist["hist_data"]["terminal_state_name"], device_info)
-            automation.automation_control(hist["device_id"], "di_change_state", hist["hist_data"]["terminal_no"], event_detail, None)
+            event_detail = diNameToEventDetail(
+                hist["hist_data"]["terminal_state_name"], device_info
+            )
+            automation.automation_control(
+                hist["device_id"],
+                "di_change_state",
+                hist["hist_data"]["terminal_no"],
+                event_detail,
+                None,
+            )
         # バッテリー残量、デバイス異常、パラメータ異常、FW更新異常
-        elif hist["hist_data"]["event_type"] in ["battery_near", "device_abnormality", "parameter_abnormality", "fw_update_abnormality"]:
-            automation.automation_control(hist["device_id"], hist["hist_data"]["event_type"], None, None, hist["hist_data"]["occurrence_flag"])
+        elif hist["hist_data"]["event_type"] in [
+            "battery_near",
+            "device_abnormality",
+            "parameter_abnormality",
+            "fw_update_abnormality",
+        ]:
+            automation.automation_control(
+                hist["device_id"],
+                hist["hist_data"]["event_type"],
+                None,
+                None,
+                hist["hist_data"]["occurrence_flag"],
+            )
         # 電源オン
-        elif hist["hist_data"]["event_type"]  == "power_on":
-            automation.automation_control(hist["device_id"], hist["hist_data"]["event_type"], None, None, 0)
+        elif hist["hist_data"]["event_type"] == "power_on":
+            automation.automation_control(
+                hist["device_id"], hist["hist_data"]["event_type"], None, None, 0
+            )
         # その他
         else:
             continue
