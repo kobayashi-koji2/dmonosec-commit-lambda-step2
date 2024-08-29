da6325632dd1520c979bcf970740594ff96b4601
Koji Kobayashi <kobayashi-koji2@design.secom.co.jp>
2024-08-29 18:29:18 +0900

    グループ名のキーワード検索でグループ一覧を取得する処理を追加

--- group-list/contents/lambda_function.py
+++ group-list/contents/lambda_function.py
@@ -2,10 +2,11 @@ import json
 import os
 import boto3
 import traceback
-
+import re
 from aws_lambda_powertools import Logger
 from botocore.exceptions import ClientError
 from aws_xray_sdk.core import patch_all
+from functools import reduce
 
 import auth
 import ssm
@@ -53,17 +54,40 @@ def lambda_handler(event, context, user):
 
         group_list = []
         try:
+            detect_condition = None
+            keyword = None
+            query_params = event.get("queryStringParameters")
+            if query_params:
+                keyword = query_params.get("keyword")
+                query_param_detect_condition = query_params.get("detect_condition")
+                if query_param_detect_condition:
+                    if query_param_detect_condition.isdecimal():
+                        detect_condition = int(query_param_detect_condition)
+
             contract_info = db.get_contract_info(user["contract_id"], contract_table)
             for group_id in contract_info.get("contract_data", {}).get("group_list", {}):
                 group_info = db.get_group_info(group_id, group_table)
-                group_list.append(
-                    {
-                        "group_id": group_id,
-                        "group_name": group_info.get("group_data", {})
-                        .get("config", {})
-                        .get("group_name", {}),
+                if detect_condition != None and keyword != None and keyword != "":
+                    get_flag = keyword_detection_group(detect_condition,keyword,group_info)
+                elif detect_condition == None and (keyword == None or keyword == ""):
+                    get_flag = True
+                else:
+                    res_body = {"message": "検索対象またはキーワードが設定されていません。"}
+                    return {
+                        "statusCode": 400,
+                        "headers": res_headers,
+                        "body": json.dumps(res_body, ensure_ascii=False),
                     }
-                )
+                
+                if get_flag:
+                    group_list.append(
+                        {
+                            "group_id": group_id,
+                            "group_name": group_info.get("group_data", {})
+                            .get("config", {})
+                            .get("group_name", {}),
+                        }
+                    )
                 logger.info(group_list)
             if group_list:
                 group_list = sorted(group_list, key=lambda x:x['group_name'])
@@ -91,3 +115,73 @@ def lambda_handler(event, context, user):
             "headers": res_headers,
             "body": json.dumps(body, ensure_ascii=False),
         }
+    
+def keyword_detection_group(detect_condition,keyword,group_info):
+
+    if detect_condition == 1:
+        #group_nameで検索
+        get_flag = group_detect(detect_condition,keyword,group_info)
+    else:
+        #無効なdetect_condition(検索条件)の場合は検索にかけない。
+        get_flag = True
+    
+    return get_flag
+
+def group_detect(detect_condition,keyword,group_info):
+
+        # AND,OR区切りでリスト化
+    if "AND" in keyword or " " in keyword or "\u3000" in keyword:
+        key_list = re.split("AND| |\u3000",keyword)
+        logger.info(f"key_list:{key_list}")
+        case = 1
+    elif "OR" in keyword:
+        key_list = re.split("OR",keyword)
+        logger.info(f"key_list:{key_list}")
+        case = 2
+    elif "-" == keyword[0]:
+        case = 3
+    else:
+        case = 0
+
+    hit_list = []
+
+    get_flag = False
+
+    if detect_condition == 1:
+        device_value = group_info.get("group_data").get("config").get("group_name")
+        logger.info(f"group_name:{device_value}")
+    else :
+        pass
+        
+    if case == 1:
+        for key in key_list:
+            if key in device_value:
+                hit_list.append(1)
+            else:
+                hit_list.append(0)
+        logger.info(f"hit_list:{hit_list}")
+        if len(hit_list)!=0:
+            result = reduce(lambda x, y: x * y, hit_list)
+            if result == 1:
+                get_flag = True
+    elif case == 2:
+        for key in key_list:
+            if key in device_value:
+                hit_list.append(1)
+            else:
+                hit_list.append(0)
+        logger.info(f"hit_list:{hit_list}")
+        if len(hit_list)!=0:
+            result = sum(hit_list)
+            if result != 0:
+                get_flag = True
+    elif case == 3:
+        if keyword[1:] in device_value:
+            pass
+        else:
+            get_flag = True
+    else:
+        if keyword in device_value:
+            get_flag = True
+
+    return get_flag
