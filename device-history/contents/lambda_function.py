962710c437eb4a2c01ef31734fd44e67b994ef82
Hyo SHIOZAWA <sts04673@design.secom.co.jp>
2024-10-08 10:11:06 +0900

    カスタムイベント通知内容修正

--- device-history/contents/lambda_function.py
+++ device-history/contents/lambda_function.py
@@ -223,11 +223,11 @@ def create_history_message(hist):
         # カスタムイベント(日時指定)
         if hist["event_type"] == "custom_datetime":
             custom_event_name = hist.get("custom_event_name")
-            msg = f"【カスタムイベントによる日時指定】\n{hist.get("time")}時点の{terminal_name}の状態は{hist["terminal_state_name"]}でした。\nこの通知は{custom_event_name}による通知です"
+            msg = f"【日時状態お知らせイベント】\n{hist.get("time")}時点の{terminal_name}の状態は{hist["terminal_state_name"]}でした。\nカスタムイベント「{custom_event_name}」"
         # カスタムイベント(継続時間指定)
         elif hist["event_type"] == "custom_timer":
             custom_event_name = hist.get("custom_event_name")
-            msg = f"【カスタムイベントによる継続時間指定】\n{terminal_name}の状態は{hist.get("elapsed_time")}分間{hist["terminal_state_name"]}でした。\nこの通知は{custom_event_name}による通知です"
+            msg = f"【状態継続お知らせイベント】\n{terminal_name}が{hist["terminal_state_name"]}に変化し、{hist.get("elapsed_time")}分間が経過しました。\nカスタムイベント「{custom_event_name}」"
          
     return msg
 
