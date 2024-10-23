475afc3532bb2941913d3becd4e194ef65f72508
Koji Kobayashi <kobayashi-koji2@design.secom.co.jp>
2024-10-23 16:18:31 +0900

    unatag状態通知受信処理における型変換の修正

--- unaconnect-receivedata/contents/event_judge.py
+++ unaconnect-receivedata/contents/event_judge.py
@@ -11,20 +11,20 @@ def eventJudge(req_body,device_current_state,device_id,signal_state):
         device_current_state = initCurrentStateInfo(req_body,device_current_state,device_id,signal_state)
     else:
         if req_body.get("dataType") == "GEOLOC":
-            if device_current_state.get("latitude_state") != Decimal(req_body.get("data").get("lat")):
+            if device_current_state.get("latitude_state") != Decimal(str(req_body.get("data").get("lat"))):
                 device_current_state["latitude_last_change_datetime"] = req_body.get("timestamp") * 1000
             device_current_state["latitude_state"] = req_body.get("data").get("lat",0)
-            if device_current_state.get("longitude_state") != Decimal(req_body.get("data").get("lng")):
+            if device_current_state.get("longitude_state") != Decimal(str(req_body.get("data").get("lng"))):
                 device_current_state["longitude_last_change_datetime"] = req_body.get("timestamp") * 1000
             device_current_state["longitude_state"] = req_body.get("data").get("lng",0)
-            if device_current_state.get("precision_state") != Decimal(req_body.get("data").get("radius",0)):
+            if device_current_state.get("precision_state") != Decimal(str(req_body.get("data").get("radius",0))):
                 device_current_state["precision_last_change_datetime"] = req_body.get("timestamp") * 1000
             device_current_state["precision_state"] = req_body.get("data").get("radius")
             device_current_state["latitude_last_update_datetime"] = req_body.get("timestamp") * 1000
             device_current_state["longitude_last_update_datetime"] = req_body.get("timestamp") * 1000
             device_current_state["precision_last_update_datetime"] = req_body.get("timestamp") * 1000
         elif req_body.get("dataType") == "DATA":
-            if device_current_state.get("battery_voltage") != Decimal(req_body.get("batteryVoltage")):
+            if device_current_state.get("battery_voltage") != Decimal(str(req_body.get("batteryVoltage"))):
                 device_current_state["battery_near_last_change_datetime"] = req_body.get("timestamp") * 1000
             device_current_state["battery_voltage"] = req_body.get("batteryVoltage")
             device_current_state["battery_near_last_update_datetime"] = req_body.get("timestamp") * 1000
